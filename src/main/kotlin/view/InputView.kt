9a633083d143d508e9e41b85fa4059c1838ebf8b
hyogeon <men1219@hanmail.net>
2020-03-07 23:31:39 +0900

    feat: OutputView

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -5,5 +5,10 @@ import java.util.*
 class InputView {
     companion object {
         private val scanner = Scanner(System.`in`)
+
+        fun inputMovieId(): Int {
+            println("## 예약할 영화를 선택하세요.");
+            return scanner.nextInt();
+        }
     }
 }
